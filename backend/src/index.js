4e5b38993bbd9cdff4efa826f57a90b00ae74974
Sachin Kumar Verma <139346749+Sverma2001@users.noreply.github.com>
2023-10-18 14:53:54 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,9 @@ const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 const router = require('./router');
-
 require('dotenv').config();
 require('./db/mongoose');
-
 const port = process.env.PORT
-
 dotenv.config();
 app.use(cors());
 app.use(express.json());

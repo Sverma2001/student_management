5a50069a05e88cd7c2484be25ffd3805b2fae5e5
Sverma2001 <sverma@argusoft.com>
2023-10-18 16:36:31 +0530

    minor changes

--- frontend/src/components/pages/TheHome.vue
+++ frontend/src/components/pages/TheHome.vue
@@ -57,7 +57,7 @@ export default {
     data() {
         return {
             searchTerm: '',
-            students: []
+            students: [],
         }
     },
     computed: {
@@ -89,7 +89,6 @@ export default {
             this.disableLogin();
         }
     },
-    
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import router from './router';
 import App from './App.vue';
 import Store from './store/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 const app = createApp(App);
 
 app.use(Store);
